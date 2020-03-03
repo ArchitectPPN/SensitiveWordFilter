a4d398d34877a3dd9c26f76fa23e80c092c50256
NiuJunQing <junqing.niu@leapoon.com>
2020-03-03 14:35:11 +0800

    new: init packbage

---
+++ Src/Sensitive/SensitiveWordFilter.php
@@ -0,0 +1,20 @@
+<?php
+namespace Sensitive;
+
+class SensitiveWordFilter
+{
+
+	/**
+	 * 输出一个字符串
+	 *
+	 * @param string $sStr
+	 *
+	 * @return string
+	 */
+	public static function test(string $sStr):string
+	{
+		echo $sStr;
+
+		return $sStr;
+	}
+}
